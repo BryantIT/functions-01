283e7dc586b45557789864db9fe45e19b491d9b7
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-21 12:33:47 -0500

    Functions vs Methods

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting!')
+}
+
+const person = {
+  name: 'Bryant',
+  greet: function greet() {
+    console.log('Hello!')
+  }
+}
+
+person.greet()
+
+startGameBtn.addEventListener('click', startGame)
