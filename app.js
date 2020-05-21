197e54191174f0d4f6d5adead3a9335687af237d
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-21 12:44:04 -0500

    Functions are Objects

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ function startGame() {
   console.log('Game is starting!')
 }
 
-const person = {
-  name: 'Bryant',
-  greet: function greet() {
-    console.log('Hello!')
-  }
-}
+// const person = {
+//   name: 'Bryant',
+//   greet: function greet() {
+//     console.log('Hello!')
+//   }
+// }
+//
+// person.greet()
 
-person.greet()
+console.dir(startGame)
 
 startGameBtn.addEventListener('click', startGame)
