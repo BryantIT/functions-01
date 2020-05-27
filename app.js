9152cc464d3fcef7b79779aa19f6994f609ddef0
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-27 12:37:39 -0500

    introducing rest parameters

--- app.js
+++ app.js
@@ -72,3 +72,24 @@ startGameBtn.addEventListener('click', () => {
   alert(message)
   gameIsRunning = false
 })
+
+// not related to game
+
+const sumUp = (...numbers) => {
+  let sum = 0
+  for (const num of numbers) {
+    sum += num
+  }
+  return sum
+}
+
+const subtractUp = function() {
+  let sum = 0
+  for (const num of arguments) { // don't use this, use rest operator
+    sum -= num
+  }
+  return sum
+}
+console.log(sumUp(1, 5, 10, -3, 6, 10))
+console.log(sumUp(1, 5, 10, -3, 6, 10, 22, 5, 8, 9, 45))
+console.log(subtractUp(1, 5, 10, -3, 6, 10, 22, 5, 8, 9, 45))
